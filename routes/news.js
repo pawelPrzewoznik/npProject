7338c67e4268ea8ca4eb6c3d700fc9814ba839ac
pawel <przew.p@gmail.com>
2019-12-30 09:21:23 +0100

    smarter links in navbar

--- routes/news.js
+++ routes/news.js
@@ -14,6 +14,7 @@ router.get('/en/Sports', ctrlNews.topEnSports)
 router.get('/en/Technology', ctrlNews.topEnTec)
 
 router.get('/', ctrlNews.topFr)
+router.get('/fr', ctrlNews.topFr)
 
 router.get('/Business', ctrlNews.topFrBusiness)
 router.get('/Divertissement', ctrlNews.topFrEnt)
